34dc5e00ddf2032ca12ce2caaaa395fd1e2ea921
Javier <jagi.ccs@gmail.com>
2020-03-02 18:47:33 +0000

    added api file to the tree

--- front-end-project/src/MainPage.js
+++ front-end-project/src/MainPage.js
@@ -1,6 +1,7 @@
 import React, { Fragment }  from 'react';
 import ImageContainer from './containers/ImageContainer'
 import Navbar from './components/navbar/Navbar'
+import API from './Api'
 
 const API = "https://pixabay.com/api/?key=15410961-50b6ae9ee64c3859d407a7eaa&q=nature&image_type=photo&pretty=true"
 
