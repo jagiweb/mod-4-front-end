8091fc1aa72937d1427d74408bbf945b17f011ca
Javier <jagi.ccs@gmail.com>
2020-03-02 17:55:00 +0000

    new component navbar

---
+++ front-end-project/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+
+    render() { 
+        return ( 
+            <div>
+                Navbar here
+            </div>
+         );
+    }
+}
+ 
+export default Navbar;
