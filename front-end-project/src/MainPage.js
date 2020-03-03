8c8b4973c521f0cdd8c3b1f5814c19201b189c21
Javier <jagi.ccs@gmail.com>
2020-03-03 14:15:53 +0000

    added signin to the tree

--- front-end-project/src/MainPage.js
+++ front-end-project/src/MainPage.js
@@ -2,8 +2,8 @@ import React, { Fragment }  from 'react';
 import ImageContainer from './containers/ImageContainer'
 import Navbar from './components/navbar/Navbar'
 import API from './Api'
+// import Api from './Api';
 
-const API = "https://pixabay.com/api/?key=15410961-50b6ae9ee64c3859d407a7eaa&q=nature&image_type=photo&pretty=true"
 
 class MainPage extends React.Component {
     constructor() {
@@ -13,19 +13,51 @@ class MainPage extends React.Component {
          }
     }
 
-    componentDidMount(){
-        fetch(API)
-            .then(resp => resp.json())
+    componentDidMount() {
+        // If we have a token in localStorage, attempt to use it to validate ourselves against the server
+        if (localStorage.token) {
+          API.validate(localStorage.token)
+          // Pass the username and token the server sends back to signIn
+            .then(json => this.signIn(json.username, json.token))
+        }
+        API.getPictures()
             .then(images => this.setState({
                 images: images.hits
             }))
-    }
+      }
+
+
+      signOut = () => {
+        this.setState({
+          username: null
+        })
+        localStorage.removeItem("token")
+      }
+    
+      signIn = (username, token) => {
+        // Set the state of username to be the username the server sent back
+        this.setState({
+          username
+        })
+        // Store the token the server sent back in localStorage, which is on the client-side
+
+        localStorage.token = token
+      }
+    
+      // Sign the user out by setting the username to null and removing the token key from localStorage
+      signOut = () => {
+        this.setState({
+          username: null
+        })
+        localStorage.removeItem("token")
+      }
+
 
     render() { 
         // console.log(this.state.images)
         return ( 
             <Fragment>
-                <Navbar/>
+                <Navbar signIn={this.signIn}/>
                 <ImageContainer images={this.state.images}/>
             </Fragment>
          );
--- front-end-project/src/components/navbar/Navbar.js
+++ front-end-project/src/components/navbar/Navbar.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import SignIn from './SignIn'
+import { Route } from 'react-router-dom'
 
 class Navbar extends React.Component {
 
     render() { 
         return ( 
-            <div>
-                Navbar here
-            </div>
+   
+            <Fragment>
+                
+                <Route exact path="/" component={() => <SignIn signIn={this.props.signIn}/>}/>
+            </Fragment>
+                
+            
+
          );
     }
 }
