22cf3e992c797aa660edc627c0d9abc7a718aa27
Amir Shariff <amirshariff@msn.com>
2020-03-04 13:42:48 +0000

    sign in moved to sidebar

--- front-end-project/src/components/navbar/Navbar.js
+++ front-end-project/src/components/navbar/Navbar.js
@@ -44,27 +44,44 @@ class Navbar extends React.Component {
     
         const { username, signOut, signUp, signIn } = this.props
         return (
-            
-            <Fragment>
+					<Fragment>
+						<div>
+							<p> {username} </p>
+							{/* <Link to="/">Home</Link>
+							<Link to="sign-in">Sign In</Link> */}
+							<div>
+								{this.props.username ? (
+									<button onClick={signOut}>Log Out</button>
+								) : null}
+							</div>
+							<Route
+								exact
+								path="/sign-in"
+								component={() => <SignIn signIn={signIn} />}
+							/>
 
-                <div>
-                    <p> {username} </p>
-                    <Link to="/">Home</Link>
-                    <Link to="sign-in">Sign In</Link>
-                    <div>{this.props.username ? <button onClick={signOut}>Log Out</button> : null}</div>
-                    <Route exact path="/sign-in" component={() => <SignIn signIn={signIn} />} />
-
-                    <Route exact path="/sign-up" component={() => <SignUp signUp={signUp} />} />
-                    <Route exact path="/sign-in" component={() => <SignIn signIn={this.props.signIn} />} />
-                    <Toolbar drawToggleClickHandler={this.drawToggleClickHandler} />
-                    <SideDrawer show={this.state.sideDraweropen}  />
-                    {backdrop}
-                    
-                </div>
-            </Fragment>
-            
-            
-        );
+							<Route
+								exact
+								path="/sign-up"
+								component={() => <SignUp signUp={signUp} />}
+							/>
+							<Route
+								exact
+								path="/sign-in"
+								component={() => <SignIn signIn={this.props.signIn} />}
+							/>
+							<Toolbar drawToggleClickHandler={this.drawToggleClickHandler} />
+							<SideDrawer
+								show={this.state.sideDraweropen}
+								signIn={signIn}
+								signUp={signUp}
+								username={username}
+								signOut={signOut}
+							/>
+							{backdrop}
+						</div>
+					</Fragment>
+				);
         
                
     };
